373dd22a2c0a51414a7ae436c8ca37de2e642fe3
Panda8Bamboo <pandasaster@gmail.com>
2024-11-22 18:14:53 +0800

    feat: add navigation to main page, update footer navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const aside = document.getElementById("navigationAside");
-    const projectsLink = document.getElementById("projectsLink");
-    const aboutLink = document.getElementById("aboutLink");
-    const contactLink = document.getElementById("contactLink");
-    const resumeLink = document.getElementById("resumeLink");
-
-    projectsLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "projects.html";
-    });
-
-    aboutLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "about.html";
-    });
-
-    contactLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "contact.html";
-    });
-
-    contactLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "resume.html";
-    });
-
-    function homeIconClick() {
-        window.location.href = 'index.html';
-    }
 
     function dashboardIconClick() {
         if (navigationAside.classList.contains("-translate-x-full")) {
@@ -39,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
             navigationAside.classList.remove("translate-x-0"); }
     }
 
-    document.getElementById("homeButton").addEventListener("click", homeIconClick);
     document.getElementById("dashboardButton").addEventListener("click", dashboardIconClick);
 });
 
