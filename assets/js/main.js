779e2a8ff18455ffaa843162cb5ef3dd31da81f0
Panda8Bamboo <pandasaster@gmail.com>
2024-11-22 15:37:43 +0800

    fix: aside transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navigationAside = document.getElementById("navigationAside");
+    const aside = document.getElementById("navigationAside");
 
-function homeIconClick() {
-    window.location.href = 'index.html';
-}
-
-function dashboardIconClick() {
-    alert("Dashboard clicked");
-    if (navigationAside.classList.contains("-translate-x-full")) {
-        navigationAside.classList.remove("-translate-x-full");
-        navigationAside.classList.add("translate-x-0");
+    function homeIconClick() {
+        window.location.href = 'index.html';
     }
-    else {
-        navigationAside.classList.add("-translate-x-full");
-        navigationAside.classList.remove("translate-x-0"); }
-}
 
-}
+    function dashboardIconClick() {
+        alert("Dashboard clicked");
+        if (navigationAside.classList.contains("-translate-x-full")) {
+            navigationAside.classList.remove("-translate-x-full");
+            navigationAside.classList.add("translate-x-0");
+        }
+        else {
+            navigationAside.classList.add("-translate-x-full");
+            navigationAside.classList.remove("translate-x-0"); }
+    }
+
+    document.getElementById("homeButton").addEventListener("click", homeIconClick);
+    document.getElementById("dashboardButton").addEventListener("click", dashboardIconClick);
+});
+
