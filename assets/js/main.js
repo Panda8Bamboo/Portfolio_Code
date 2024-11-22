5afc284aec15ed54978f2d3a52b4b5b422017079
Panda8Bamboo <pandasaster@gmail.com>
2024-11-22 13:27:41 +0800

    feat: begin adding aside navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navigationAside = document.getElementById("navigationAside");
+
 function homeIconClick() {
     window.location.href = 'index.html';
 }
 
-function gridIconClick() {
-    alert("test");
+function dashboardIconClick() {
+    alert("Dashboard clicked");
+    if (navigationAside.classList.contains("-translate-x-full")) {
+        navigationAside.classList.remove("-translate-x-full");
+        navigationAside.classList.add("translate-x-0");
+    }
+    else {
+        navigationAside.classList.add("-translate-x-full");
+        navigationAside.classList.remove("translate-x-0"); }
+}
+
 }
