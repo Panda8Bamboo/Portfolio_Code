7ac90837998a27537e4455fff5a0822594f05b96
Panda8Bamboo <pandasaster@gmail.com>
2024-11-30 10:18:36 +0800

    feat: implement show/hide button on resume page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    /* Reveal the dashboard on dashboard icon click */
     const aside = document.getElementById("navigationAside");
     document.getElementById("dashboardButton").addEventListener("click", dashboardIconClick);
-    const phrases = ["Amber Howe", "2D Artist", "Web Developer", "Programmer"];
-    const textElement = document.getElementById("flashing-text");
-    let index = 0;
-
     function dashboardIconClick() {
         if (navigationAside.classList.contains("-translate-x-full")) {
             navigationAside.classList.remove("-translate-x-full");
@@ -15,6 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
             navigationAside.classList.remove("translate-x-0"); }
     }
 
+    /* Flash the tagline text in the header */
+    const phrases = ["Amber Howe", "2D Artist", "Web Developer", "Programmer"];
+    const textElement = document.getElementById("flashing-text");
+    let index = 0;
     function updateText() {
         textElement.classList.remove("opacity-100");
         textElement.classList.add("opacity-0");
@@ -27,7 +29,23 @@ document.addEventListener("DOMContentLoaded", () => {
             textElement.classList.add("opacity-100");
         }, 500);
     }
-
     setInterval(updateText, 3000);
+
+    /* Reveal more button in the resume section */
+    const readMoreContainers = document.querySelectorAll(".read-more-container");
+
+    readMoreContainers.forEach(container => {
+        const readMoreBtn = container.querySelector(".read-more-btn");
+        const hiddenText = container.querySelector(".hidden");
+
+        readMoreBtn.addEventListener("click", function () {
+            hiddenText.classList.toggle("hidden");
+            if (hiddenText.classList.contains("hidden")) {
+                readMoreBtn.textContent = "Read More";
+            } else {
+                readMoreBtn.textContent = "Read Less";
+            }
+        });
+    });
 });
 
