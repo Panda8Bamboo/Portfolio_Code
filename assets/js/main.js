ef5177933284a7e44e1778f79ea5efba04a17c8f
Panda8Bamboo <pandasaster@gmail.com>
2024-12-02 20:24:42 +0800

    feat: update header and footer styling all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     /* Reveal the dashboard on dashboard icon click */
-    const aside = document.getElementById("navigationAside");
-    document.getElementById("dashboardButton").addEventListener("click", dashboardIconClick);
-    function dashboardIconClick() {
-        if (navigationAside.classList.contains("-translate-x-full")) {
-            navigationAside.classList.remove("-translate-x-full");
-            navigationAside.classList.add("translate-x-0");
+    const navigationAside = document.getElementById("navigationAside");
+    const dashboardButton = document.getElementById("dashboardButton");
+
+    if (navigationAside && dashboardButton) {
+        dashboardButton.addEventListener("click", dashboardIconClick);
+
+        function dashboardIconClick() {
+            if (navigationAside.classList.contains("-translate-x-full")) {
+                navigationAside.classList.remove("-translate-x-full");
+                navigationAside.classList.add("translate-x-0");
+            } else {
+                navigationAside.classList.add("-translate-x-full");
+                navigationAside.classList.remove("translate-x-0");
+            }
         }
-        else {
-            navigationAside.classList.add("-translate-x-full");
-            navigationAside.classList.remove("translate-x-0"); }
     }
+
+
 
     /* Flash the tagline text in the header */
     const phrases = ["Amber Howe", "2D Artist", "Web Developer", "Programmer"];
     const textElement = document.getElementById("flashing-text");
     let index = 0;
-    function updateText() {
-        textElement.classList.remove("opacity-100");
-        textElement.classList.add("opacity-0");
+    if (textElement) {
+        function updateText() {
+            textElement.classList.remove("opacity-100");
+            textElement.classList.add("opacity-0");
 
-        setTimeout(() => {
-            textElement.textContent = phrases[index];
-            index = (index + 1) % phrases.length;
+            setTimeout(() => {
+                textElement.textContent = phrases[index];
+                index = (index + 1) % phrases.length;
 
-            textElement.classList.remove("opacity-0");
-            textElement.classList.add("opacity-100");
-        }, 500);
+                textElement.classList.remove("opacity-0");
+                textElement.classList.add("opacity-100");
+            }, 500);
+        }
+        setInterval(updateText, 3000);
+    } else {
+        console.warn('Element with ID "flashing-text" not found. UpdateText will not run.');
     }
-    setInterval(updateText, 3000);
+
 
     /* Reveal more button in the resume section */
     const readMoreContainers = document.querySelectorAll(".read-more-container");
